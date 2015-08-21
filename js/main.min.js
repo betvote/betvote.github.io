043be612d65804665ee3819e6bd48a232bae540c
r-montagner <r_montagner@aol.com>
2015-08-21 06:58:22 -0300

    minified update

---
+++ js/main.min.js
@@ -0,0 +1,1 @@
+!function(){$("#menuToggle, .menu-close").on("click",function(){$("#menuToggle").toggleClass("active"),$("body").toggleClass("body-push-toleft"),$("#theMenu").toggleClass("menu-open")})}(jQuery);
