0f770357785ecda5917ee4e2c385043911da4460
Betvote <betvote@users.noreply.github.com>
2015-09-28 05:22:55 -0300

    update listas & youtube id´s

--- js/Candidatos.js
+++ js/Candidatos.js
@@ -3,11 +3,11 @@ candidatos = null;
 function CargarCandidatos() {
 	candidatos = [
 	// ID_CANDIDATO_PARTIDO, NOMBRE_CANDIDATO, LISTA_CANDIDATO, IMAGEN_CANDIDATO, PARTIDO_CANDIDATO, TWITTER_CANDIDATO, ADDRESS_CANDIDATO, BET_DEPRECATED, YOUTUBE, IMAGEN2
-	Candidato(0, 'Daniel Scioli', 'A LISTA CELESTE Y BLANCA K NESTOR KIRCHNER', 'img/candidates/scioli.jpg', 0, '@danielscioli', address_a, "", "e1T3ZjCMN1g", 'img/candidates/scioli2.jpg'),
-	Candidato(0, 'Mauricio Macri', 'LISTA G EL CAMINO DEL CAMBIO', 'img/candidates/macri.jpg', 1,'@mauriciomacri', address_b, "", "NjSi98FEDOQ", 'img/candidates/macri2.jpg'), 
-	Candidato(0, 'Sergio Massa', 'A FRENTE RENOVADOR +A15', 'img/candidates/massa.jpg', 5, '@sergiomassa', address_c, "", "Uv20fCw7j84", 'img/candidates/massa2.jpg'),
-	Candidato(0, 'Nicolás del Caño', 'LISTA 1 A RENOVAR Y FORTALECER', 'img/candidates/delcano.jpg', 2, '@NicolasdelCano', address_d, "", "6lhGXfNYaV4", 'img/candidates/delcano2.jpg'), 
-	Candidato(0, 'Margarita Stolbizer', 'LISTA IGUALDAD Y DECENCIA MS', 'img/candidates/stolbizer.jpg', 4, '@Stolbizer', address_e, "", "eCYyPIIUuNQ", 'img/candidates/stolbizer2.jpg'),
-	Candidato(0, 'Adolfo Rodríguez Saá', 'LISTA A 1', 'img/candidates/saa.jpg', 3, '@AdolfoRS2015', address_f, "", "_D5Cg2pPugE", 'img/candidates/saa2.jpg')
+	Candidato(0, 'Daniel Scioli', 'LISTA 131', 'img/candidates/scioli.jpg', 0, '@danielscioli', address_a, "", "TLkTmk9T8FA", 'img/candidates/scioli2.jpg'),
+	Candidato(0, 'Mauricio Macri', 'LISTA 135', 'img/candidates/macri.jpg', 1,'@mauriciomacri', address_b, "", "SkWhce2IZUE", 'img/candidates/macri2.jpg'), 
+	Candidato(0, 'Sergio Massa', 'LISTA 138', 'img/candidates/massa.jpg', 5, '@sergiomassa', address_c, "", "Uv20fCw7j84", 'img/candidates/massa2.jpg'),
+	Candidato(0, 'Nicolás del Caño', 'LISTA 137', 'img/candidates/delcano.jpg', 2, '@NicolasdelCano', address_d, "", "WKNONOW4AVg", 'img/candidates/delcano2.jpg'), 
+	Candidato(0, 'Margarita Stolbizer', 'LISTA 132', 'img/candidates/stolbizer.jpg', 4, '@Stolbizer', address_e, "", "eCYyPIIUuNQ", 'img/candidates/stolbizer2.jpg'),
+	Candidato(0, 'Adolfo Rodríguez Saá', 'LISTA 133', 'img/candidates/saa.jpg', 3, '@AdolfoRS2015', address_f, "", "_D5Cg2pPugE", 'img/candidates/saa2.jpg')
 	];
 }
