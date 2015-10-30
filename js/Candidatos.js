e8420952a7d6bf61290aaf4f3639430d0d5b7b78
Betvote <betvote@users.noreply.github.com>
2015-10-30 03:45:15 -0300

    #BALLOTAGE UPDATE

--- js/Candidatos.js
+++ js/Candidatos.js
@@ -4,10 +4,6 @@ function CargarCandidatos() {
 	candidatos = [
 	// ID_CANDIDATO_PARTIDO, NOMBRE_CANDIDATO, LISTA_CANDIDATO, IMAGEN_CANDIDATO, PARTIDO_CANDIDATO, TWITTER_CANDIDATO, ADDRESS_CANDIDATO, BET_DEPRECATED, YOUTUBE, IMAGEN2
 	Candidato(0, 'Daniel Scioli', 'LISTA 131', 'img/candidates/scioli.jpg', 0, '@danielscioli', address_a, "", "TLkTmk9T8FA", 'img/candidates/scioli2.jpg'),
-	Candidato(0, 'Mauricio Macri', 'LISTA 135', 'img/candidates/macri.jpg', 1,'@mauriciomacri', address_b, "", "tqj6Cc9bwmc", 'img/candidates/macri2.jpg'), 
-	Candidato(0, 'Sergio Massa', 'LISTA 138', 'img/candidates/massa.jpg', 5, '@sergiomassa', address_c, "", "amNQRAlxNlA", 'img/candidates/massa2.jpg'),
-	Candidato(0, 'Nicolás del Caño', 'LISTA 137', 'img/candidates/delcano.jpg', 2, '@NicolasdelCano', address_d, "", "WKNONOW4AVg", 'img/candidates/delcano2.jpg'), 
-	Candidato(0, 'Margarita Stolbizer', 'LISTA 132', 'img/candidates/stolbizer.jpg', 4, '@Stolbizer', address_e, "", "4oaDL7ea78I", 'img/candidates/stolbizer2.jpg'),
-	Candidato(0, 'Adolfo Rodríguez Saá', 'LISTA 133', 'img/candidates/saa.jpg', 3, '@AdolfoRS2015', address_f, "", "_D5Cg2pPugE", 'img/candidates/saa2.jpg')
+	Candidato(0, 'Mauricio Macri', 'LISTA 135', 'img/candidates/macri.jpg', 1,'@mauriciomacri', address_b, "", "tqj6Cc9bwmc", 'img/candidates/macri2.jpg')
 	];
 }
