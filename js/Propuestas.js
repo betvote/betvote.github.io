c1be65d1639e92ec099645a028c79b7f2e918700
Betvote <betvote@users.noreply.github.com>
2015-10-30 03:46:48 -0300

    #BALLOTAGE UPDATE

--- js/Propuestas.js
+++ js/Propuestas.js
@@ -6,13 +6,5 @@ propuestas = [
 Propuesta("", "", temas.TRANSPORTE, 0, 0),
 //
 Propuesta("", "", temas.TRANSPORTE, 1, 0),
-//
-Propuesta("", "", temas.TRANSPORTE, 2, 0),
-//
-Propuesta("", "", temas.TRANSPORTE, 3, 0),
-//
-Propuesta("", "", temas.TRANSPORTE, 4, 0),
-//
-Propuesta("", "", temas.TRANSPORTE, 5, 0),
 ];
 }
