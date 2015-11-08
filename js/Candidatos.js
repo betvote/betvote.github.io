fe02bd0b279de610c157481ae60c905ff1e01341
Betvote <betvote@users.noreply.github.com>
2015-11-08 07:16:48 -0300

    Update Candidatos.js

--- js/Candidatos.js
+++ js/Candidatos.js
@@ -4,6 +4,6 @@ function CargarCandidatos() {
 	candidatos = [
 	// ID_CANDIDATO_PARTIDO, NOMBRE_CANDIDATO, LISTA_CANDIDATO, IMAGEN_CANDIDATO, PARTIDO_CANDIDATO, TWITTER_CANDIDATO, ADDRESS_CANDIDATO, BET_DEPRECATED, YOUTUBE, IMAGEN2
 	Candidato(0, 'Daniel Scioli', 'LISTA 131', 'img/candidates/scioli.jpg', 0, '@danielscioli', address_a, "", "TLkTmk9T8FA", 'img/candidates/scioli2.jpg'),
-	Candidato(0, 'Mauricio Macri', 'LISTA 135', 'img/candidates/macri.jpg', 1,'@mauriciomacri', address_b, "", "tqj6Cc9bwmc", 'img/candidates/macri2.jpg')
+	Candidato(0, 'Mauricio Macri', 'LISTA 135', 'img/candidates/macri.jpg', 1,'@mauriciomacri', address_b, "", "s_W9_ILUCR0", 'img/candidates/macri2.jpg')
 	];
 }
